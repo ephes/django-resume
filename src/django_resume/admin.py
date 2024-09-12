9ef5441286222f48a41808eac881c22bf2e9cd24
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-09-12 21:09:28 +0200

    added admin for person

--- src/django_resume/admin.py
+++ src/django_resume/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Person
+
+admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    fields = ("name", "slug", "plugin_data")
