7b133ccc7b55063c0542efb2389671b4a242d449
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-09-12 18:41:46 +0200

    better settings, make pytest work in activated venv + some gitignore stuff

--- tests/settings.py
+++ tests/settings.py
@@ -7,3 +7,39 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "resume.apps.ResumeConfig",
 ]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "tests/test_database.sqlite3",
+        # if this is not set, an in memory database is used
+        # for tests by default _get_test_db_name
+        "TEST": {"NAME": "tests/test_database.sqlite3"},
+    },
+}
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
