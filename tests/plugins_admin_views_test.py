6e4ad1ad47bb9e70d5ca495a8cfca573696d49f3
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-09-17 12:27:30 +0200

    test for timeline plugin admin update

--- tests/plugins_admin_views_test.py
+++ tests/plugins_admin_views_test.py
@@ -54,16 +54,50 @@ def test_timeline_plugin_create_item(admin_client, person, timeline_item_data):
     assert item["badges"] == badges_list
 
 
-@pytest.mark.django_db
-def test_timeline_plugin_delete_item(admin_client, person, timeline_item_data):
-    # Given a person in the database with a timeline item
+@pytest.fixture
+def person_with_timeline_item(timeline_item_data):
+    person: Person = Person(name="John Doe", slug="john-doe")
     timeline_item_data["id"] = "123"
+    plugin = TimelinePlugin()
     form = TimelineItemForm(data=timeline_item_data, person=person)
     assert form.is_valid()
-
-    plugin = TimelinePlugin()
     person = plugin.create(person, form.cleaned_data)
     person.save()
+    return person
+
+
+@pytest.mark.django_db
+def test_timeline_plugin_update_item(
+    admin_client, person_with_timeline_item, timeline_item_data
+):
+    # Given a person in the database with a timeline item
+    person: Person = person_with_timeline_item
+    plugin = TimelinePlugin()
+
+    plugin_data = plugin.get_data(person)
+    [item] = plugin_data["items"]
+    timeline_item_data["id"] = item["id"]
+    timeline_item_data["role"] = "Updated Developer"
+    update_url = plugin.get_admin_change_item_post_url(person.id)
+
+    # When we update the timeline item
+    r = admin_client.post(update_url, timeline_item_data)
+
+    # Then the response should be successful
+    assert r.status_code == 200
+
+    # And the item should be updated in the database
+    person.refresh_from_db()
+    plugin_data = plugin.get_data(person)
+    [item] = plugin_data["items"]
+    assert item["role"] == timeline_item_data["role"]
+
+
+@pytest.mark.django_db
+def test_timeline_plugin_delete_item(admin_client, person_with_timeline_item):
+    # Given a person in the database with a timeline item
+    person: Person = person_with_timeline_item
+    plugin = TimelinePlugin()
 
     # When we delete the timeline item
     delete_url = plugin.get_admin_delete_item_url(person.id, "123")
