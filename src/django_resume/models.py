6525be5464575fdc6ee0fa0d701b4a01943d453e
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-10-10 12:31:02 +0200

    use {} if plugin_data is null

--- src/django_resume/models.py
+++ src/django_resume/models.py
@@ -4,10 +4,15 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    plugin_data = models.JSONField(default=dict)
+    plugin_data = models.JSONField(default=dict, blank=True, null=False)
 
     def __repr__(self):
         return f"<{self.name}>"
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.plugin_data is None:
+            self.plugin_data = {}
+        super().save(*args, **kwargs)
