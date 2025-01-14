a1aae22cea8a64217a2fd2c83ba97dcaeb091a4d
Jochen Wersdörfer <jochen@wersdoerfer.de>
2025-01-14 14:27:25 +0100

    #2 lowercase looks better

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Django Resume"
+project = "django-resume"
 copyright = "2025, Jochen Wersdörfer"
 author = "Jochen Wersdörfer"
 release = "0.1.11"
