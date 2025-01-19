7719b162210c1ec32dec188a66911d00ea45387a
Jochen Wersdörfer <jochen@wersdoerfer.de>
2025-01-19 13:54:37 +0100

    #2 trying to get rid of warnings

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ release = "0.1.11"
 extensions = []
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "requirements.txt"]
 
 
 # -- Options for HTML output -------------------------------------------------
